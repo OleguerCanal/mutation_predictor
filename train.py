e76f85ed3582684bc78d6682fa8c70bbd6e07770
OleguerCanal <oleguer.canal@hotmail.com>
2022-08-07 09:36:21 +0200

    Bug

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from model.mutation_predictor import MutationPredictor
 set_seed(0)
 
 if __name__=="__main__":
-    datamodule = MutationsDatamodule(config=config)
+    datamodule = MutationsDatamodule(dataset_path="data/data.csv", batch_size=10)
     wandb_logger = WandbLogger(project="mutation_predictor")
 
     trainer = pl.Trainer(max_epochs=10, accelerator="gpu", logger=wandb_logger)
